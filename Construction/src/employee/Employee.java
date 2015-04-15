cc8a24eadfe132484eb3dbd761ca5fd6ea7fe74d
kaylanpace <kaylanpace@yahoo.com>
2015-04-15 00:50:32 -0600

    Employee entity enhancements;

--- Construction/src/employee/Employee.java
+++ Construction/src/employee/Employee.java
@@ -1,34 +1,58 @@
-//robert wuz here
 package employee;
 
 import java.io.Serializable;
+
 import javax.persistence.*;
 
-//import workorder.WorkOrder;
+import workorder.WorkOrder;
+
+
 
 @Entity
+@Table(name="Employee")
 public class Employee implements Serializable {
 	private static final long serialVersionUID = 1L;
 	 
     // Persistent Fields:
-    @Id @GeneratedValue
-    Long id;
+    @Id @GeneratedValue @Column
+    Long empId;
+    
+    @Column
     private String empFirstName;
+    
+    @Column
     private String empLastName;
+    
+    @Column(unique=true, nullable=false)
     private int ssn;
+    
+    @Column
     private String position;
+    
+    @Column
     private int age;
+    
+    @Column
     private boolean isAdmin;
    
-    /*@ManyToOne(optional=true)
-    @JoinColumn(name="workorderId")
-    public WorkOrder workorder;*/
- 
+	
+    
+   //Employee and WorkOrder Table join.
+    @ManyToOne(optional=true)
+    @JoinColumn(name="workorderId", nullable=true, updatable=false)
+    public workorder.WorkOrder workorder;
+    
+   
+    
+   
     // Constructors:
     public Employee() {
+    	
     }
+    
+  
  
-    public Employee(String empFirstName, String empLastName, int ssn, String position, int age, boolean isAdmin ) {
+    public Employee(String empFirstName, String empLastName, int ssn, String position, int age, boolean isAdmin) {
        
         this.empFirstName = empFirstName;
         this.empLastName = empLastName;
@@ -36,20 +60,107 @@ public class Employee implements Serializable {
         this.position = position;
         this.age = age;
         this.isAdmin = isAdmin;
+        
     }
+    
+    //Access Methods
  
-   /* public WorkOrder getWorkOrder() {
+    public WorkOrder getWorkOrder() {
         return workorder;
-      }
-
-      public void setWorkOrder(WorkOrder workorder) {
+    }
+    
+    public void setWorkOrder(WorkOrder workorder) {
         this.workorder = workorder;
-      }*/
-      
-    // String Representation:
+    }
+
+
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+
+
+	public String getEmpFirstName() {
+		return empFirstName;
+	}
+
+
+
+	public void setEmpFirstName(String empFirstName) {
+		this.empFirstName = empFirstName;
+	}
+
+
+
+	public String getEmpLastName() {
+		return empLastName;
+	}
+
+
+
+	public void setEmpLastName(String empLastName) {
+		this.empLastName = empLastName;
+	}
+
+
+
+	public int getSsn() {
+		return ssn;
+	}
+
+
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+
+
+	public String getPosition() {
+		return position;
+	}
+
+
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+	// String Representation:
     @Override
     public String toString() {
-        return id + " " + empFirstName + " " + empLastName
-        		+ " " + ssn + " " + position + " " + age + " " + isAdmin;
+        return empId + " " + empFirstName + " " + empLastName
+        		+ " " + ssn + " " + position + " " + age + " " + isAdmin + " " + getWorkOrder();
     }
 }
